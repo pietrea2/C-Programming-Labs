b4cfe8a2615805c722b27d9af8e94554c1b892ec
Adam Pietrewicz <adam.pietrewicz@mail.utoronto.ca>
2023-09-10 02:24:54 -0400

    First commit for lab0. Testing.

---
+++ Lab 0/lab0.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	
+    printf("This is my Lab 0 program!\n");
+
+    printf("\n\nIf at first ");
+    printf("you don't succeed" ",\n");
+    printf("failure may be ");
+    printf("your style\n");
+    
+    
+    printf("\nA slash is \"/\"\n");
+    printf("while\n");
+    printf("a backslash is \"\\\"\n");
+
+
+    return 0;
+    
+
+}
